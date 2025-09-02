021519f82e2c4053894df0ff31be67f5b63f3ef3
AiyazMd12 <aiyaz.mohammed12@gmail.com>
2025-09-02 17:02:52 +0000

    thumbnail

--- src/components/FourSectionLayout.tsx
+++ src/components/FourSectionLayout.tsx
@@ -18,7 +18,7 @@ const FourSectionLayout = () => {
             <article key={id} className="group">
               <div className="relative overflow-hidden rounded">
                 <img
-                   src={`/src/assets/${thumbnail}`}
+                   src={`${thumbnail}`}
                   alt={title}
                   className="w-full h-80 sm:h-96 object-cover object-top  /* keep faces visible */
              transition-transform duration-300 group-hover:scale-105"
